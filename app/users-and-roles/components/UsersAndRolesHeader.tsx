c54ed9d7f9b0d7f16442fa55de7a1cf6c5d33760
NexusProgrammers <jawadprogrammer@gmail.com>
2025-10-09 13:10:15 -0700

    fixed

--- app/users-and-roles/components/UsersAndRolesHeader.tsx
+++ app/users-and-roles/components/UsersAndRolesHeader.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Plus, Building2, User } from "lucide-react";
+import {  Building2, User } from "lucide-react";
 
 interface UsersAndRolesHeaderProps {
   onCreateUserClick: () => void;
